94a196fa9112933d8516b37c2540a1457e569838
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-08-12 16:44:27 +0900

    writing simple excel files success!

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,34 @@
-use calamine::{open_workbook, Error, Xlsx, Reader, RangeDeserializerBuilder};
+#[macro_use]
+extern crate simple_excel_writer;
+use simple_excel_writer as excel;
+
+use excel::*;
 
 fn main() {
+    let mut wb    = Workbook::create("tmp/b.xlsx");
+    let mut sheet = wb.create_sheet("SheetName");
+
+    sheet.add_column(Column { width: 30.0 });
+    sheet.add_column(Column { width: 30.0 });
+    sheet.add_column(Column { width: 80.0 });
+    sheet.add_column(Column { width: 60.0 });
+
+    wb.write_sheet(&mut sheet, |sheet_writer| {
+        let sw = sheet_writer;
+        sw.append_row(row!["Name", "Title","Success","XML Remark"])?;
+        sw.append_row(row!["Amy", (), true,"<xml><tag>\"Hello\" & 'World'</tag></xml>"])?;
+        sw.append_blank_rows(2);
+        sw.append_row(row!["Tony", blank!(30), "retired"])
+    }).expect("write excel error!");
+
+    let mut sheet = wb.create_sheet("Sheet2");
+    wb.write_sheet(&mut sheet, |sheet_writer| {
+        let sw = sheet_writer;
+        sw.append_row(row!["Name", "Title","Success","Remark"])?;
+        sw.append_row(row!["Amy", "Manager", true])
+    }).expect("write excel error!");
+
+    wb.close().expect("close excel error!");
+
     println!("Hello, world!");
 }
