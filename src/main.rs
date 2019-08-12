a04f1e561c3952dd0d31c306cb271099cb73453e
Kei Moomin Sugano <tobasojyo@gmail.com>
2019-08-12 16:50:22 +0900

    refacotring

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-#[macro_use]
 extern crate simple_excel_writer;
 use simple_excel_writer as excel;
+use std::io;
 
 use excel::*;
 
@@ -13,22 +13,18 @@ fn main() {
     sheet.add_column(Column { width: 80.0 });
     sheet.add_column(Column { width: 60.0 });
 
-    wb.write_sheet(&mut sheet, |sheet_writer| {
-        let sw = sheet_writer;
-        sw.append_row(row!["Name", "Title","Success","XML Remark"])?;
-        sw.append_row(row!["Amy", (), true,"<xml><tag>\"Hello\" & 'World'</tag></xml>"])?;
-        sw.append_blank_rows(2);
-        sw.append_row(row!["Tony", blank!(30), "retired"])
-    }).expect("write excel error!");
-
-    let mut sheet = wb.create_sheet("Sheet2");
-    wb.write_sheet(&mut sheet, |sheet_writer| {
-        let sw = sheet_writer;
-        sw.append_row(row!["Name", "Title","Success","Remark"])?;
-        sw.append_row(row!["Amy", "Manager", true])
-    }).expect("write excel error!");
+    wb.write_sheet(
+        &mut sheet,
+        |sheet_writer| {
+            let sw = sheet_writer;
+            sw.append_row(row!["Name", "Title", "Success", "XML Remark"])?;
+            sw.append_row(row!["Amy",  (),      true,      "AAA"       ])?;
+            sw.append_blank_rows(2);
+            sw.append_row(row!["Tony", blank!(30), "retired"           ])
+        }
+    )
+    .expect("write excel error!");
 
     wb.close().expect("close excel error!");
-
     println!("Hello, world!");
 }
